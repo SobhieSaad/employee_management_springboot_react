cbaedce1585e4594d011a2692184f383afdfcbe7
Sobhie <sobhie.saad1@gmail.com>
2022-04-20 23:57:10 +0300

    fetch employees list

---
+++ employees_frontend/employees_frontend/src/components/ListEmployeeComponent.jsx
@@ -0,0 +1,55 @@
+
+import React, { Component } from 'react'
+import EmployeeService from '../services/EmployeeService'
+
+export class ListEmployeeComponent extends Component {
+
+    constructor (props){
+        super(props)
+        this.state={
+            employees: []
+        }
+    }
+
+    componentDidMount(){
+        EmployeeService.getEmployees().then((res)=>{
+            this.setState({
+                employees: res.data
+            });
+        });
+    }
+  render() {
+    return (
+      <div>
+          <h2 className='text-center'>Employees List</h2> 
+          <div className='row'>
+              <table className='table table-bordered table-striped'>
+                <thead>
+                    <tr>
+                        <th>First name</th>
+                        <th>Last name</th>
+                        <th>Email</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        this.state.employees.map(
+                            employee=>
+                            <tr key={employee.id}>
+                                <td>{employee.firstName}</td>
+                                <td>{employee.lastName}</td>
+                                <td>{employee.emailId}</td>
+                                <td></td>
+                            </tr>
+                        )
+                    }
+                </tbody>
+              </table>
+          </div>
+      </div>
+    )
+  }
+}
+
+export default ListEmployeeComponent
--- employees_springboot_backend/src/main/java/net/javaguids/controller/EmployeeController.java
+++ employees_springboot_backend/src/main/java/net/javaguids/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import net.javaguids.model.Employee;
 import net.javaguids.repository.EmployeeRepository;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
