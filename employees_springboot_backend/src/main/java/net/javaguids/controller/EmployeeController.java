c6db088733d37d881239b66d09cff97f2915e6ad
Sobhie <sobhie.saad1@gmail.com>
2022-04-20 18:12:42 +0300

    first commit

---
+++ employees_springboot_backend/src/main/java/net/javaguids/controller/EmployeeController.java
@@ -0,0 +1,31 @@
+package net.javaguids.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.javaguids.model.Employee;
+import net.javaguids.repository.EmployeeRepository;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @GetMapping("/employees/{id}")
+    public Optional<Employee> getEmployee(String id) {
+        return employeeRepository.findById(Long.parseLong(id));
+    }
+
+}
