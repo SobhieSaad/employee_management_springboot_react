1e10729f2b6194f97e20991e589519a3b5a496f6
Sobhie <sobhie.saad1@gmail.com>
2022-05-05 21:32:24 +0300

    configure routes

--- employees_frontend/employees_frontend/src/App.js
+++ employees_frontend/employees_frontend/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router , Route, Switch} from 'react-router-dom';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterCompnent from './components/FooterCompnent';
@@ -7,11 +8,18 @@ import FooterCompnent from './components/FooterCompnent';
 function App() {
   return (
     <div>
-      <HeaderComponent />
-      <div className="container">
-        <ListEmployeeComponent />
-      </div>
-      <FooterCompnent />
+      <Router>
+          <HeaderComponent />
+          <div className="container">
+            <Switch>
+              <Route path='/' component={ListEmployeeComponent} ></Route>
+              <Route path='/employees' component={ListEmployeeComponent} ></Route>
+
+              <ListEmployeeComponent />
+            </Switch>
+          </div>
+          <FooterCompnent />
+      </Router>
     </div>
 
   );
